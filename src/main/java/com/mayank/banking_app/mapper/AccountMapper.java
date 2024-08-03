e11ed4d9d80b05a69cdcc38a31cf04cd3858d2e2
mayank-2711 <minku2711@gmail.com>
2024-08-03 23:48:52 +0530

    Initial commit

---
+++ src/main/java/com/mayank/banking_app/mapper/AccountMapper.java
@@ -0,0 +1,24 @@
+package com.mayank.banking_app.mapper;
+
+import com.mayank.banking_app.dto.AccountDto;
+import com.mayank.banking_app.entity.Account;
+
+public class AccountMapper {
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance()
+        );
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+        return accountDto;
+    }
+}
---
+++ src/main/java/com/mayank/banking_app/service/AccountService.java
@@ -0,0 +1,22 @@
+package com.mayank.banking_app.service;
+
+import com.mayank.banking_app.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id);
+
+    List<AccountDto> bulkUploadAccounts(List<AccountDto> accountDtos);
+
+}
